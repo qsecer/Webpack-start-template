db8a6c139ceb25d9691271029f3fc05b3c7ed9e9
qsecer <dimagolovin169@gmail.com>
2024-12-10 22:15:35 +0500

    234

---
+++ src/js/button--button.js
@@ -0,0 +1,6 @@
+const buttonBurger = document.querySelector('.button-burger');
+const hideBurger = document.querySelector('.hidden-burger');
+const asideMenu = document.querySelector('.aside');
+let activeFunc = function(){
+    
+}
