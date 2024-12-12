ae7e1631352c7b5a649b5aac2640af32892506d8
qsecer <dimagolovin169@gmail.com>
2024-12-12 14:21:05 +0500

    789

--- src/js/swiper2.js
+++ src/js/swiper2.js
@@ -1,35 +1,35 @@
 const viewBtn = document.querySelector('.view-table__button');
-const hiddenItems = document.querySelector('.view-table-item');
-let classOfButton = viewBtn.classList;
-console.log()
+const hiddenItems = document.getElementsByClassName('view-table__item');
+const width = window.innerWidth;
+
 document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    
     if(width >=768 && width < 1439){
         
-        button.addEventListener('click', ()=>{
-            let classOfButton = button.classList;
-            viewBtn.addEventListener('click', ()=>{
-                
-                if(classOfButton[1] === 'show-more'){
-                    for(let i = 6; i < hiddenItems.length; i++){
-                        hiddenItems[i].style.display = 'flex';
-                        viewBtn.classList.add('hide');
-                        viewBtn.classList.remove('show-more');
-                        viewBtn.textContent = 'Скрыть';
-                    }
-                }else{
-                    for(let i =8; i < hiddenItems.length; i++){
-                        hiddenItems[i].style.display = 'none';
-                        viewBtn.classList.add('show-more');
-                        viewBtn.classList.remove('hide');
-                        viewBtn.textContent = 'Показать больше';
-                    }
-                }})
-        })
-
-
-    }if(width >=1440){
         viewBtn.addEventListener('click', ()=>{
-            
+            let classOfButton = viewBtn.classList;
+            if(classOfButton[1] === 'show-more'){
+                for(let i = 6; i < hiddenItems.length; i++){
+                    hiddenItems[i].style.display = 'flex';
+                    viewBtn.classList.add('hide');
+                    viewBtn.classList.remove('show-more');
+                    viewBtn.textContent = 'Скрыть';
+                }
+            }else{
+                for(let i = 6; i < hiddenItems.length; i++){
+                    hiddenItems[i].style.display = 'none';
+                    viewBtn.classList.add('show-more');
+                    viewBtn.classList.remove('hide');
+                    viewBtn.textContent = 'Показать больше';
+                }
+            }
+        })
+    }
+    if(width >=1440){
+        
+        viewBtn.addEventListener('click', ()=>{
+            let classOfButton = viewBtn.classList;
             if(classOfButton[1] === 'show-more'){
                 for(let i = 8; i < hiddenItems.length; i++){
                     hiddenItems[i].style.display = 'flex';
@@ -44,9 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     viewBtn.classList.remove('hide');
                     viewBtn.textContent = 'Показать больше';
                 }
-            }})
+            }
+        })
     }
-
-
 })
-
