2820b5793805256c4725fe39e29d77a79379a153
qsecer <dimagolovin169@gmail.com>
2024-12-12 22:07:30 +0500

    q

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.html';
 import './index.scss';
 import './js/swiper';
-import './js/button--button';
 import './js/swiper2';
 import './js/swiper3';
-import './js/modal-call';
+import './js/button--button';
+// import './js/modal-call';
