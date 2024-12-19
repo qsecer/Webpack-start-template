28768a4e2f5e40637760d0d341ad55924c8bb52a
qsecer <dimagolovin169@gmail.com>
2024-12-20 01:04:01 +0500

    1

--- src/js/buty.js
+++
@@ -1,5 +0,0 @@
-const fch = document.querySelector('.nav__link');
-const item = document.querySelector('nav__item');
-document.addEventListener('DOMContentLoaded', () => {
-    fch.style.border = '2px solid #41F6D7';
-})
