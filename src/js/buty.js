1eaf0ecda9118aea886297cf798cbf191bba3cce
qsecer <dimagolovin169@gmail.com>
2024-12-20 01:20:17 +0500

    111

---
+++ src/js/buty.js
@@ -0,0 +1,7 @@
+const link = document.querySelector('nav__link');
+const item = document.querySelector('aside__item')
+let func = function(){
+    link.classList.add('active');
+    item.classList.add('active');
+}
+document.addEventListener('DOMContentLoaded',)
