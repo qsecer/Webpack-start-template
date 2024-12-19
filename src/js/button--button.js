9563377f53488ad16d7affdce58d5b03c762d706
qsecer <dimagolovin169@gmail.com>
2024-12-19 14:27:42 +0500

    1111

--- src/js/button--button.js
+++ src/js/button--button.js
@@ -1,7 +1,23 @@
 const buttonBurger = document.querySelector('.button-burger');
 const asideMenu = document.querySelector('.aside');
+let dontScroll = document.body.style.position = '';
+let body = document.querySelector('body')
 let activeFunc = function(){
     buttonBurger.classList.toggle("active")
     asideMenu.classList.toggle("active")
+    if (asideMenu.classList[1] === "active"){
+        body.style.height = '100%';
+        body.style.overflow = 'hidden'
+    }
+    if(asideMenu.classList[1] !== "active"){
+        body.style.overflow = 'scroll'
+    }
 }
-buttonBurger.addEventListener('click', activeFunc)
+buttonBurger.addEventListener('click', activeFunc);
+
+// if (asideMenu.classList[1] === "active"){
+//     dontScroll = document.body.style.position = 'fixed'
+// }
+// if(asideMenu.classList[1] !== "active"){
+//     dontScroll = document.body.style.position = ''
+// }
